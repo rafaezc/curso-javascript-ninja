a591d7f2577dfd63afb7b862da06c484572789f2
Rafael Estevan Zubiolo Cavalaro <rafaelzubiolo151@gmail.com>
2020-08-20 13:26:31 -0300

    Resolução Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -22,19 +22,26 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+> function retArray(x, y) {
+... return x[y];
+... }
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+> var arrFive = [false, -0, 25, 'rafael', NaN]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
 
+> console.log(retArray(arrFive, 0)); //false
+> console.log(retArray(arrFive, 1)); //-0
+> console.log(retArray(arrFive, 2)); //25
+> console.log(retArray(arrFive, 3)); //'rafael'
+> console.log(retArray(arrFive, 4)); //NaN
+  
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
@@ -49,30 +56,38 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book (x) {
+var nomeLivros = {'CasaDePapel': {qtdPaginas: 385, autor: 'Abc', editora: 'PnC'}, 
+'BolaDeNeve':{qtdPaginas: 385, autor: 'Abc', editora: 'PnC'}, 
+'Zero':{qtdPaginas: 385, autor: 'Abc', editora: 'PnC'}};
+return x === undefined ? nomeLivros : nomeLivros[x];
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+> book();
+{ CasaDePapel: { qtdPaginas: 385, autor: 'Abc', editora: 'PnC' },
+  BolaDeNeve: { qtdPaginas: 385, autor: 'Abc', editora: 'PnC' },
+  Zero: { qtdPaginas: 385, autor: 'Abc', editora: 'PnC' } }
+>
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+> console.log('O livro ' + nomeLivros + ' tem ' + book('Zero').qtdPaginas + ' páginas!'); //O livro Zero tem 385 páginas!
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+> console.log('O autor do livro ' + nomeLivros + ' é ' + book('Zero').autor + '!'); //O autor do livro Zero é Abc!
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+> console.log('O livro ' + nomeLivros + ' foi publicado pela editora ' + book('Zero').editora + '!'); //O livro Zero foi publicado pela editora PnC!
