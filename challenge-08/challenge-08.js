a75f1b730f9f55c13b4681965a9b51f5ae347dad
Rafael Estevan Zubiolo Cavalaro <rafaelzubiolo151@gmail.com>
2020-08-25 16:01:49 -0300

    Resolução Challenge 08

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -85,7 +85,7 @@ VAR
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-> var sum = calculator('+')
+> var sum = calculator('+');
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
@@ -97,10 +97,10 @@ Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-> var subtraction = calculator('-')
-> var multiplication = calculator('*')
-> var division = calculator('/')
-> var module = calculator('%')
+> var subtraction = calculator('-');
+> var multiplication = calculator('*');
+> var division = calculator('/');
+> var module = calculator('%');
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
